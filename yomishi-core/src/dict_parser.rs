f1e13c2e37fd6f0a02c66681cc3424cf67403690
naipofo <50967056+naipofo@users.noreply.github.com>
2023-08-19 13:42:43 +0200

    parse dicts from files

--- yomishi-core/src/dict_parser.rs
+++ yomishi-core/src/dict_parser.rs
@@ -1,5 +1,10 @@
-use std::collections::VecDeque;
+use std::{
+    collections::VecDeque,
+    fs::{read_dir, read_to_string},
+    path::Path,
+};
 
+use regex::Regex;
 use serde::Deserialize;
 use serde_json::Value;
 
@@ -22,24 +27,20 @@ pub struct Term {
     term_tags: String,
 }
 
-pub fn parse_dict(index: &str, bank: &str) -> (String, Vec<Term>) {
-    // TODO: parse other categories besides terms
-    let DictIndex { title, format } = serde_json::from_str(index).unwrap();
+fn parse_bank(format: i32, bank: &str) -> Vec<Term> {
     // TODO: convert from v1
     if format != 3 {
         todo!("No support for format other than 3")
     }
-    let entries: Vec<VecDeque<Value>> = serde_json::from_str(bank).unwrap();
 
-    (
-        title,
-        entries
-            .into_iter()
-            .map(convert_term_v3)
-            .collect::<Result<_, _>>()
-            .unwrap(),
-    )
+    serde_json::from_str::<Vec<_>>(bank)
+        .unwrap()
+        .into_iter()
+        .map(convert_term_v3)
+        .collect::<Result<_, _>>()
+        .unwrap()
 }
+
 fn convert_term_v3(v: VecDeque<Value>) -> serde_json::Result<Term> {
     let mut v = v;
     Ok(Term {
@@ -52,4 +53,24 @@ fn convert_term_v3(v: VecDeque<Value>) -> serde_json::Result<Term> {
         sequence: serde_json::from_value(v.pop_front().unwrap())?,
         term_tags: serde_json::from_value(v.pop_front().unwrap())?,
     })
+}
+
+pub fn import_from_path(index_path: &Path) -> std::io::Result<(String, Vec<Term>)> {
+    let index = read_to_string(index_path)?;
+    let DictIndex { title, format } = serde_json::from_str(&index).unwrap();
+
+    let files = read_dir(index_path.parent().unwrap())?;
+    let re = Regex::new(r"term\_bank\_(\d+)\.json").unwrap();
+
+    let terms = files
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| re.is_match(&e.file_name().to_string_lossy()))
+        .map(|f| Ok(parse_bank(format, &read_to_string(f.path())?)))
+        .collect::<Result<Vec<_>, std::io::Error>>()?
+        .into_iter()
+        .flatten()
+        .collect();
+
+    Ok((title, terms))
 }
--- yomishi-core/src/main.rs
+++ yomishi-core/src/main.rs
@@ -7,22 +7,20 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
+
     use crate::{
         deinflector::{deinflect, deinflect_all},
-        dict_parser::parse_dict,
+        dict_parser::import_from_path,
     };
 
     #[test]
     fn test_yomishi() {
-        let index = include_str!("../../local_test_files/index.json");
-        let terms = include_str!("../../local_test_files/term_bank_1.json");
+        let (title, dict) = import_from_path(Path::new("../local_test_files/index.json")).unwrap();
+        println!("imported \"{title}\" with {} terms", dict.len());
 
         let deinf = include_str!("../../local_test_files/deinflect.json");
-
-        let dict = parse_dict(index, terms);
-
         let deinf_list = serde_json::from_str(deinf).unwrap();
-
         let di = deinflect(&deinf_list, "食べませんでした");
         println!("{:?}", di);
 
