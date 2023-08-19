93a44851f228976d488535e81710465653b25586
naipofo <50967056+naipofo@users.noreply.github.com>
2023-08-19 15:18:54 +0200

    support for format v1

--- yomishi-core/src/dict_parser.rs
+++ yomishi-core/src/dict_parser.rs
@@ -28,15 +28,15 @@ pub struct Term {
 }
 
 fn parse_bank(format: i32, bank: &str) -> Vec<Term> {
-    // TODO: convert from v1
-    if format != 3 {
-        todo!("No support for format other than 3")
-    }
-
     serde_json::from_str::<Vec<_>>(bank)
         .unwrap()
         .into_iter()
-        .map(convert_term_v3)
+        .map(if format == 1 {
+            convert_term_v1
+        } else {
+            convert_term_v3
+        })
+        .map(|e| e.map(fill_reading))
         .collect::<Result<_, _>>()
         .unwrap()
 }
@@ -53,6 +53,34 @@ fn convert_term_v3(v: VecDeque<Value>) -> serde_json::Result<Term> {
         sequence: serde_json::from_value(v.pop_front().unwrap())?,
         term_tags: serde_json::from_value(v.pop_front().unwrap())?,
     })
+}
+
+fn convert_term_v1(v: VecDeque<Value>) -> serde_json::Result<Term> {
+    let mut v = v;
+    Ok(Term {
+        expression: serde_json::from_value(v.pop_front().unwrap())?,
+        reading: serde_json::from_value(v.pop_front().unwrap())?,
+        definition_tags: serde_json::from_value(v.pop_front().unwrap())?,
+        rules: serde_json::from_value(v.pop_front().unwrap())?,
+        score: serde_json::from_value(v.pop_front().unwrap())?,
+        glossary: v
+            .into_iter()
+            .map(serde_json::from_value)
+            .collect::<Result<_, _>>()?,
+        sequence: 0,
+        term_tags: "".to_string(),
+    })
+}
+
+fn fill_reading(t: Term) -> Term {
+    if t.reading.len() > 0 {
+        t
+    } else {
+        Term {
+            reading: t.expression.clone(),
+            ..t
+        }
+    }
 }
 
 pub fn import_from_path(index_path: &Path) -> std::io::Result<(String, Vec<Term>)> {
