904cc40c1518478c417c386792760d9e576473f5
naipofo <50967056+naipofo@users.noreply.github.com>
2023-08-26 17:29:07 +0200

    very basic text scanning

--- yomishi-ts/extension/content-script/content-script.ts
+++ yomishi-ts/extension/content-script/content-script.ts
@@ -1,4 +1,29 @@
 console.log("yomishi init!");
 document.body.addEventListener("mousemove", (e) => {
-  const _hoveredElement = document.elementFromPoint(e.clientX, e.clientY);
+    const hoveredString = getStringFromCaret(e.clientX, e.clientY);
+    console.log(hoveredString);
 });
+
+function getStringFromCaret(x: number, y: number): string {
+    const anyDocument = document as any;
+    let range: any;
+    let offset: number;
+    let textNode;
+
+    if (typeof anyDocument.caretRangeFromPoint === "function") {
+        // Chromium
+        range = anyDocument.caretRangeFromPoint(x, y);
+        offset = range.startOffset;
+        textNode = range.startContainer;
+    } else if (typeof anyDocument.caretPositionFromPoint === "function") {
+        // Firefox
+        range = anyDocument.caretPositionFromPoint(x, y);
+        offset = range.offset;
+        textNode = range.offsetNode;
+    } else {
+        throw new Error("carret support not found!");
+    }
+    let data = textNode.textContent as string;
+    // TODO: break node boudaries
+    return data.substring(offset, data.length);
+}
